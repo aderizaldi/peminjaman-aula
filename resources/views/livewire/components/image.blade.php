c6083162c267701b459966f6cf6429fc291bffdc
Ade Rizaldi <ade.rizaldi.mm2@gmail.com>
2025-10-22 11:05:08 +0700

    fix: bug home

--- resources/views/livewire/components/image.blade.php
+++ resources/views/livewire/components/image.blade.php
@@ -1,5 +1,7 @@
 <div class="w-full h-full flex flex-col items-center justify-center" wire:poll.3600s="updateImage">
     <div class="relative w-full h-0 overflow-hidden" style="padding-bottom: 56.25%;">
+        @if($images)
         <img src="storage/{{ $images[$currentIndex] }}" wire:poll.5000ms="nextImage" alt="Slideshow" class="absolute top-0 left-0 w-full h-full object-contain">
+        @endif
     </div>
 </div>
